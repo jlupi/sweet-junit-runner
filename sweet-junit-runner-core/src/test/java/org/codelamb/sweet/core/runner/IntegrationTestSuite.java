62c1cd5261aa0e35906cc8c377d48e1693a31fdc
Lukasz Pielak <pielak@gmail.com>
2017-04-25 10:53:59 +0200

    removed redundant mvn module, licence change

--- sweet-junit-runner-core/src/test/java/org/codelamb/sweet/core/runner/IntegrationTestSuite.java
+++
@@ -1,9 +0,0 @@
-
-package org.codelamb.sweet.core.runner;
-
-import org.junit.runner.RunWith;
-
-@RunWith(SuiteRunner.class)
-@SuiteConfiguration("org.codelamb.sweet.core")
-public class IntegrationTestSuite {
-}
