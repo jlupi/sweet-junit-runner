a035e2919d3aaf39db072673399f7ab33fedfcf9
Michael Krausse <michael.krausse@directbox.com>
2017-10-10 20:25:00 +0200

    jlupie/ehmka  - implementing https://github.com/jlupi/sweet-junit-runner/issues/6

--- sweet-junit-runner/src/main/java/org/codelamb/sweet/runner/SuiteComponentsScanner.java
+++ sweet-junit-runner/src/main/java/org/codelamb/sweet/runner/SuiteComponentsScanner.java
@@ -1,7 +1,11 @@
-
 package org.codelamb.sweet.runner;
 
 import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Modifier;
@@ -11,47 +15,69 @@ import java.util.Set;
 
 class SuiteComponentsScanner {
 
-    private final String packageToScan;
+  private final String packageToScan;
+  private String containsFilter;
 
-    public SuiteComponentsScanner(String packageName) {
-        if (packageName == null || packageName.isEmpty()) {
-            throw new IllegalArgumentException("packageName must be provided");
-        }
-
-        this.packageToScan = packageName;
+  public SuiteComponentsScanner(String packageName, String containsFilter) {
+    if (packageName == null || packageName.isEmpty()) {
+      throw new IllegalArgumentException("packageName must be provided");
     }
 
-    public Set<Class<?>> loadClasses() {
-        Reflections reflections = new Reflections(packageToScan);
-        Set<Class<?>> result = new LinkedHashSet<>();
-        result.addAll(getClasses(reflections, IntegrationTest.class));
-        result.addAll(getClasses(reflections, SuiteTest.class));
-        return result;
-    }
+    this.packageToScan = packageName;
+    this.containsFilter = containsFilter;
+  }
 
-    private Set<Class<?>> getClasses(Reflections reflections, Class klass) {
-        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(klass);
-        for (Iterator<Class<?>> iterator = annotatedClasses.iterator(); iterator.hasNext();) {
-            Class<?> annotatedClass = iterator.next();
-            Annotation annotation =  annotatedClass.getAnnotation(klass);
-            if (Modifier.isAbstract(annotatedClass.getModifiers())) {
-                iterator.remove();
-            } else if (!isEnabledInSuite(annotation)) {
-                iterator.remove();
-            }
+  public Set<Class<?>> loadClasses() {
+    Reflections reflections = //new Reflections(  packageToScan);
+            new Reflections(
+                    new ConfigurationBuilder()
+                            .setUrls(ClasspathHelper.forPackage(packageToScan))
+                            .setScanners(new SubTypesScanner(false), new TypeAnnotationsScanner())
+                            .filterInputsBy(new FilterBuilder().includePackage(packageToScan)));
 
-        }
-        return annotatedClasses;
-    }
+    Set<Class<?>> result = new LinkedHashSet<>();
+    result.addAll(getClasses(reflections));
+    return result;
+  }
 
-    private boolean isEnabledInSuite(Annotation annotation) {
-        if  (annotation instanceof IntegrationTest) {
-            return ((IntegrationTest) annotation).enabledInSuite();
-        }
-        if  (annotation instanceof SuiteTest) {
-            return ((SuiteTest) annotation).enabledInSuite();
-        }
-        return false;
+  private Set<Class<?>> getClasses(Reflections reflections) {
+    Set<Class<?>> annotatedClasses = reflections.getSubTypesOf(Object.class);
+    for (Iterator<Class<?>> iterator = annotatedClasses.iterator(); iterator.hasNext(); ) {
+      Class<?> annotatedClass = iterator.next();
+
+      if (Modifier.isAbstract(annotatedClass.getModifiers())) {
+        iterator.remove();
+      } else if (!matchesPatternOrIsEnabledSuite(annotatedClass)) {
+        iterator.remove();
+      }
     }
+    return annotatedClasses;
+  }
+
+  private boolean matchesPatternOrIsEnabledSuite(Class annotatedClass) {
+    return isAnnotatedAndEnabledInSuite(annotatedClass) || matchesPattern(annotatedClass);
+  }
+
+  private boolean matchesPattern(Class annotatedClass) {
+    if (containsFilter.trim().equals("")) {
+      return false;
+    }
+    if (annotatedClass.getCanonicalName().toLowerCase().contains(containsFilter)) {
+      return true;
+    }
+    return false;
+  }
+
+  private boolean isAnnotatedAndEnabledInSuite(Class klass) {
+    for (Annotation annotation : klass.getAnnotations()) {
+      if (annotation instanceof IntegrationTest) {
+        return ((IntegrationTest) annotation).enabledInSuite();
+      }
+      if (annotation instanceof SuiteTest) {
+        return ((SuiteTest) annotation).enabledInSuite();
+      }
+    }
+    return false;
+  }
 
 }
--- sweet-junit-runner/src/test/java/org/codelamb/sweet/runner/SuiteComponentsScannerTest.java
+++ sweet-junit-runner/src/test/java/org/codelamb/sweet/runner/SuiteComponentsScannerTest.java
@@ -1,4 +1,3 @@
-
 package org.codelamb.sweet.runner;
 
 import org.codelamb.sweet.runner.testclasses.*;
@@ -11,35 +10,79 @@ import static org.junit.Assert.*;
 
 public class SuiteComponentsScannerTest {
 
-    private static final String PACKAGE_NAME = "org.codelamb.sweet";
+  private static final String PACKAGE_NAME = "org.codelamb.sweet";
 
-    @Test
-    public void loadAnnotatedClasses() {
-        // given
-        SuiteComponentsScanner sut = new SuiteComponentsScanner(PACKAGE_NAME);
+  @Test
+  public void loadAnnotatedClasses() {
+    // given
+    SuiteComponentsScanner sut = new SuiteComponentsScanner(PACKAGE_NAME, "");
 
-        // when
-        Set<Class<?>> allClasses = sut.loadClasses();
+    // when
+    Set<Class<?>> allClasses = sut.loadClasses();
 
-        // then
-        assertTrue(allClasses.contains(LoadedIntegrationTest.class));
-        assertTrue(allClasses.contains(LoadedSuiteTest.class));
-        assertFalse(allClasses.contains(IgnoredNotAnnotatedTest.class));
-        assertFalse(allClasses.contains(IgnoredDisabledTest.class));
-        assertFalse(allClasses.contains(IgnoredAbstractTest.class));
+    // then
+    assertTrue(allClasses.contains(LoadedIntegrationTest.class));
+    assertTrue(allClasses.contains(LoadedSuiteTest.class));
+    assertFalse(allClasses.contains(IgnoredNotAnnotatedTest.class));
+    assertFalse(allClasses.contains(IgnoredDisabledTest.class));
+    assertFalse(allClasses.contains(ClassWithoutAnnotationTest.class));
+    assertFalse(allClasses.contains(IgnoredAbstractTest.class));
 
-        for (Class<?> allClass : allClasses) {
-            Annotation annotation = allClass.getAnnotation(IntegrationTest.class);
-            if (annotation == null) {
-                annotation = allClass.getAnnotation(SuiteTest.class);
-            }
-            assertNotNull("Didn't find required annotation on class " + allClass.getSimpleName(), annotation);
-        }
+    for (Class<?> allClass : allClasses) {
+      Annotation annotation = allClass.getAnnotation(IntegrationTest.class);
+      if (annotation == null) {
+        annotation = allClass.getAnnotation(SuiteTest.class);
+      }
+      assertNotNull("Didn't find required annotation on class " + allClass.getSimpleName(), annotation);
     }
+  }
 
+  @Test
+  public void loadAnnotatedClassesWithContainsFilter() {
+    // given
+    SuiteComponentsScanner sut = new SuiteComponentsScanner(PACKAGE_NAME, "without");
 
-    @Test(expected = IllegalArgumentException.class)
-    public void throwsExceptionWhenModuleNameNotProvided() {
-        new SuiteComponentsScanner(null);
+    // when
+    Set<Class<?>> allClasses = sut.loadClasses();
+
+    // then
+    assertTrue(allClasses.contains(LoadedIntegrationTest.class));
+    assertTrue(allClasses.contains(LoadedSuiteTest.class));
+    assertTrue(allClasses.contains(ClassWithoutAnnotationTest.class));
+    assertFalse(allClasses.contains(IgnoredNotAnnotatedTest.class));
+    assertFalse(allClasses.contains(IgnoredDisabledTest.class));
+    assertFalse(allClasses.contains(IgnoredAbstractTest.class));
+
+  }
+
+  @Test
+  public void loadAnnotatedClassesWithContainsFilter2() {
+    // given
+    SuiteComponentsScanner sut = new SuiteComponentsScanner(PACKAGE_NAME, "hkjdahjkdashjkhjkdas");
+
+    // when
+    Set<Class<?>> allClasses = sut.loadClasses();
+
+    // then
+    assertTrue(allClasses.contains(LoadedIntegrationTest.class));
+    assertTrue(allClasses.contains(LoadedSuiteTest.class));
+    assertFalse(allClasses.contains(IgnoredNotAnnotatedTest.class));
+    assertFalse(allClasses.contains(IgnoredDisabledTest.class));
+    assertFalse(allClasses.contains(ClassWithoutAnnotationTest.class));
+    assertFalse(allClasses.contains(IgnoredAbstractTest.class));
+
+    for (Class<?> allClass : allClasses) {
+      Annotation annotation = allClass.getAnnotation(IntegrationTest.class);
+      if (annotation == null) {
+        annotation = allClass.getAnnotation(SuiteTest.class);
+      }
+      assertNotNull("Didn't find required annotation on class " + allClass.getSimpleName(), annotation);
     }
+  }
+
+
+  @Test(expected = IllegalArgumentException.class)
+  public void throwsExceptionWhenModuleNameNotProvided() {
+    new SuiteComponentsScanner(null, null);
+  }
 }
